7cfbabbc6663e8b3ea7f8415675af32f296ae909
Goutam Choudhary <goutamchoudhary4053@gmail.com>
2020-08-02 12:11:00 +0530

    commit 2

--- app.py
+++ app.py
@@ -63,15 +63,15 @@ def prediction(review):
 def home():
   return render_template("index.html")
 
-@app.route('/templates/index.html')
+@app.route('/Home')
 def index():
   return render_template("index.html")
 
-@app.route('/templates/about.html')
+@app.route('/about')
 def about():
   return render_template("about.html")
 
-@app.route('/templates/contact.html')
+@app.route('/contact')
 def contact():
   return render_template("contact.html")
 
--- util.py
+++ util.py
@@ -6,11 +6,11 @@ import json
 import pandas as pd
 import numpy as np
 import nltk
-#nltk.download('stopwords')
+nltk.download('stopwords')
 
 
-#sw = set(stopwords.words('english'))
-stopwords = nltk.corpus.stopwords.words('english')
+sw = set(stopwords.words('english'))
+#stopwords = nltk.corpus.stopwords.words('english')
 ps = PorterStemmer()
 
 # ps.stem("jumping")        # It reduces the size of the vocabulary
@@ -23,7 +23,7 @@ def clean_text(sample):
     
     sample = sample.split()
     
-    sample = [ps.stem(s) for s in sample if s not in stopwords] 
+    sample = [ps.stem(s) for s in sample if s not in sw] 
     # List Comprehension -> Doing this it will remove all the stopwords from sample
     
     sample = " ".join(sample)
